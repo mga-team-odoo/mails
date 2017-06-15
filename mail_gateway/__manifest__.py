0175e145f7480e1471f3e8be591595066a6c4a77
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-06-15 11:48:40 +0200

    Add new module to manage incoming mail
    The mail_gateway module will use a new controller to handle interference
    with the mail gateway. The existing gateway uses the admin user and
    password by default, preventing it from changing easily.
    The new gateway will use a token, as it is retrieved in many APIs

---
+++ mail_gateway/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2017 Christophe CHAUVET <christophe.chauvet@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "New gateway to manage incomming mail",
+    "version": "10.0.1.0.0",
+    "author": "Christophe CHAUVET",
+    "license": "LGPL-3",
+    "category": "Social Network",
+    "summary": """Additionnal API to manage API
+    Better than the existing one that use admin account and hardocde password
+    New API use a Token as Authentication, better to manage multiple database and instance
+    """,
+    "depends": [
+        'mail',
+        #'document',
+    ],
+    "data": [],
+    "installable": True,
+}
